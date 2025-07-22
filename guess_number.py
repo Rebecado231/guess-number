63bd3c5f1f8f5ab41ed8386305fba570dafe1db1
Rebecado231 <rebeca.cado@gmail.com>
2025-07-22 18:41:07 +0200

    juego final

--- guess_number.py
+++ guess_number.py
@@ -14,12 +14,15 @@ UPPER_BOUND = 100
 def generate_secret() -> int:
     """Devuelve un número aleatorio entre LOWER_BOUND y UPPER_BOUND (incluidos)."""
     random_number = random.randint(1, 100)
-    
+
     return random_number
 
+
 def ask_name() -> str:
+    """Devuelve el nombre del jugador"""
     user_name = input("User name: ")
     return user_name
+
 
 def get_user_guess() -> int:
     """Solicita al usuario un número y lo devuelve como int."""
@@ -34,8 +37,8 @@ def get_user_guess() -> int:
             print("That's not a valid number. Please try again.")
 
 
-def main() -> int:
-    """Bucle principal del juego."""
+def play_one_game() -> int:
+    """Función que representa una partida del juego"""
     attempt_num = 0
     real_num = generate_secret()
     while True:
@@ -48,36 +51,53 @@ def main() -> int:
         else:
             break
     print("Congrats! You get it just in " + str(attempt_num) + " attempts.")
-    play_again = input("Play again? Y/N")
-    if play_again.strip().upper() == "Y":
-        main()
-    else:
-        print("Bye bye!")
     return attempt_num
 
 
+def main() -> tuple[str, list[int]]:
+    """Bucle principal del juego."""
+    attempts = []
+    name = ask_name()
+    while True:
+        game_attempts = play_one_game()
+        attempts.append(game_attempts)
+        play_again = input("Play again? Y/N")
+        if play_again.strip().upper() == "N":
+            print("Bye bye!")
+            break
+    return name, attempts
+
+
 if __name__ == "__main__":
-    
-    attempts = main() 
-    att_record = []
-    att_record.append(attempts)
-    total_att = sum(att_record)
-    att_average = total_att / len(att_record)
+    player_name, all_attempts = main()
+    with open("game_record.json", "r", encoding="utf-8") as file:
+        game_record = json.load(file)
+    for player_sheet in game_record:
+        if player_sheet["player"] == player_name:
+            total_record = player_sheet["attempts"]["record"]
+            total_record.extend(all_attempts)
+            total_att = sum(total_record)
+            player_sheet["attempts"]["total"] = total_att
+            att_average = total_att / len(total_record)
+            player_sheet["attempts"]["average"] = att_average
+            best_play = min(total_record)
+            player_sheet["attempts"]["best"] = best_play
+            break
+    else:
+        total_att = sum(all_attempts)
+        player_sheet = {
+            "player": player_name,
+            "attempts": {
+                "record": all_attempts,
+                "total": total_att,
+                "average": total_att / len(all_attempts),
+                "best": min(all_attempts),
+            },
+        }
+        game_record.append(player_sheet)
+
+    with open("game_record.json", "w", encoding="utf-8") as file:
+        json.dump(game_record, file, indent="\t")
+        print("Game saved")
 
 
-
-player_sheet = {
-    "player" : "user_name"
-    "attempts" : [] 
-    }
-
-def sheet_register (att_record, total_att, att_average):
-    attempts = {
-        "Record" = att_record
-        "Total" = total_att 
-        "Average" = att_average
-    }
-
-with open (game_record.json, "w" ) as file:
-        json.dump (player_sheet, file)
-print ("Game saved")
